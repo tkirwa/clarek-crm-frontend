5913bb46bc10b5f45f23488408c121f180f5e9c3
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-12-17 19:13:03 +0300

    Edit landing page info

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './App.css'; // Assuming you have a CSS file for styling
+
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header';
 import PrivateRoutes from './utils/PrivateRoutes';
@@ -9,14 +11,14 @@ import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className='container mx-auto max-w-screen-xl'>
       <Header/>
-      <LandingPage/>
           <Routes>
             <Route element={<PrivateRoutes />}>
                 {/* <Route element={<Home/>} path="/" exact/> */}
                 {/* <Route element={<Products/>} path="/products"/> */}
             </Route>
+            <Route element={<LandingPage/>} path="/"/>
             <Route element={<Login/>} path="/login"/>
             <Route element={<Signup/>} path="/signup"/>
           </Routes>
